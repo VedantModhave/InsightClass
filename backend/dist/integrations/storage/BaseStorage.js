d906cf00f1b294bba6f044517028682512006a20
VedantModhave <modhavevedant@gmail.com>
2026-02-22 21:37:48 +0530

    chore: add .gitignore files and remove sensitive data from git tracking

--- backend/dist/integrations/storage/BaseStorage.js
+++
@@ -1,6 +0,0 @@
-export class BaseStorage {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    getRawFile(params) {
-        throw new Error('Method not implementation');
-    }
-}
